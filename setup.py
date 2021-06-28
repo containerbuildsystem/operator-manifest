b76373ae9e5d045d5a73a102630d46d2e659b52c
Martin Basti <mbasti@redhat.com>
2021-06-28 17:54:39 +0200

    Update supported python
    Only py3.6, py3.7, py3.8 and py3.9 is supported
    
    Signed-off-by: Martin Basti <mbasti@redhat.com>

--- setup.py
+++ setup.py
@@ -15,12 +15,13 @@ setup(
     ],
     classifiers=[
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     license="BSD-3-Clause",
-    python_requires='>=3.5',
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'operator-manifest = operator_manifest.cli:main',
